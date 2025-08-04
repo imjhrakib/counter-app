b033c272548d5b04a7d9d6213d1354cfc3896099
jhr <iamjhrakib@gmail.com>
2025-08-04 06:55:05 +0600

    added counter app features

---
+++ main.js
@@ -0,0 +1,17 @@
+let count=0;
+function updateDisplay() {
+  document.getElementById('count').innerHTML = count;
+}
+function increase() {
+  count++;
+  updateDisplay();
+}
+function decrease(){
+  count--;
+  updateDisplay();
+}
+
+function reset(){
+  count=0;
+  updateDisplay();
+}
